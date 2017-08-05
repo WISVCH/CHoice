449f93879b2f1fdfaa84c4a47099e5b2b34a2270
Sille Kamoen <sille.kamoen@gmail.com>
2017-08-05 14:06:18 +0200

    Commented TestRunner class to fix Spring Context load

--- src/test/kotlin/ch/wisv/choice/ApplicationTest.kt
+++ src/test/kotlin/ch/wisv/choice/ApplicationTest.kt
@@ -1,27 +1,21 @@
 package ch.wisv.choice
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.test.context.ActiveProfiles
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @ActiveProfiles("test")
 class ApplicationTest {
 
-    @Component
-    inner class TestRunner : CommandLineRunner {
-
-        override fun run(vararg evt: String) {
-
-        }
-
-    }
+//    @Component
+//    inner class TestRunner : CommandLineRunner {
+//
+//        override fun run(vararg evt: String) {
+//
+//        }
+//
+//    }
 
     fun main(args: Array<String>) {
         SpringApplication.run(ApplicationTest::class.java, *args)
