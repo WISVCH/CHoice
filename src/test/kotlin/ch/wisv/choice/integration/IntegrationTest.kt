c327582489db303c11a79d6ad306092e40aaf527
Matthijs Kok <matthijs.kok@me.com>
2017-08-17 18:09:07 +0200

    Test configuration fixes.

--- src/test/kotlin/ch/wisv/choice/integration/IntegrationTest.kt
+++ src/test/kotlin/ch/wisv/choice/integration/IntegrationTest.kt
@@ -5,6 +5,7 @@ import io.restassured.RestAssured
 import io.restassured.config.RedirectConfig
 import org.junit.Before
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
@@ -15,8 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner
 @ActiveProfiles("test")
 abstract class IntegrationTest {
 
-    //@Value("\${local.server.port}")
-    var port: Int = 9002
+    @Value("\${local.server.port}")
+    var port: Int = 0
 
     @Before
     fun setXAuthIntegrationTest() {
