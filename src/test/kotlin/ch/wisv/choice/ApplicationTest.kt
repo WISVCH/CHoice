d006ff036f2e3d6a8230bce7ee24f333cbc88689
Matthijs Kok <matthijs.kok@me.com>
2017-08-18 19:03:59 +0200

    TestRunner works again :)

--- src/test/kotlin/ch/wisv/choice/ApplicationTest.kt
+++ src/test/kotlin/ch/wisv/choice/ApplicationTest.kt
@@ -1,23 +1,25 @@
 package ch.wisv.choice
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.stereotype.Component
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootApplication
 @ActiveProfiles("test")
 class ApplicationTest {
 
-//    @Component
-//    inner class TestRunner : CommandLineRunner {
-//
-//        override fun run(vararg evt: String) {
-//
-//        }
-//
-//    }
-
     fun main(args: Array<String>) {
         SpringApplication.run(ApplicationTest::class.java, *args)
+    }
+
+    @Component
+    class TestRunner : CommandLineRunner {
+
+        override fun run(vararg evt: String) {
+
+        }
+
     }
 }
