8cd8a3b25904b2dda22e68749d9c7c8fc825e319
Sven Popping <sven@popping.co>
2018-04-24 20:21:32 +0200

    Disable csrf and add endpoints for login and auth check

--- src/main/kotlin/ch/wisv/choice/course/controller/CourseController.kt
+++ src/main/kotlin/ch/wisv/choice/course/controller/CourseController.kt
@@ -28,10 +28,7 @@ import ch.wisv.choice.util.CHoiceException
 import ch.wisv.choice.util.ResponseEntityBuilder.Companion.createResponseEntity
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.stream.Collectors
 import javax.servlet.http.HttpServletRequest
 
